4cf63dab33b12255cb4bafd576f847a94adcaba5
Mit'ka Kuznetsov <www.kuznetsovsky@gmail.com>
2022-07-12 23:13:38 +0300

    Создает главную страницу

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,20 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+// Others
+import MainPage from '../../pages/main-page/main-page';
+
+// Types
+type AppProps = {
+  films: {
+    id:string;
+    name: string;
+    preview: string;
+  }[];
+}
+
+// Component
+function App({
+  films
+}: AppProps): JSX.Element {
+  return <MainPage films={films} />;
 }
 
 export default App;
