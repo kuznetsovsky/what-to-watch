f2e5ad6c73c9d52335bd23575aa032fc43a91a4b
Mit'ka Kuznetsov <www.kuznetsovsky@gmail.com>
2022-07-13 23:33:47 +0300

    Создает приватные маршруты

---
+++ project/src/components/private-route/private-route.tsx
@@ -0,0 +1,22 @@
+// Core
+import { Navigate } from 'react-router-dom';
+
+// Others
+import { AppRoute, AuthorizationStatus } from '../../consts';
+
+type PrivateRouteProps = {
+  authorizationStatus: AuthorizationStatus;
+  children: JSX.Element;
+}
+
+function PrivateRoute(props: PrivateRouteProps): JSX.Element {
+  const { authorizationStatus, children } = props;
+
+  return (
+    authorizationStatus === AuthorizationStatus.Auth
+      ? children
+      : <Navigate to={AppRoute.Login} />
+  );
+}
+
+export default PrivateRoute;
