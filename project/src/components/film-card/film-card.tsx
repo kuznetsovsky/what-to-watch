f74750261e2de1ac316d61ae83d042e7326bbd4a
Mit'ka Kuznetsov <www.kuznetsovsky@gmail.com>
2022-07-14 22:15:11 +0300

    Создает сохранение активной карточки с фильмом

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,25 +1,47 @@
+// Core
+import { MouseEvent } from 'react';
+
 // Others
 import { FilmType } from '../../types/film-types';
 
 // Types
 type SmallFilmCardProps = {
   film: FilmType;
+  changeSelectedFilm: (id: string | null) => void;
 }
 
 // Component
 const FilmCard = ({
-  film
-}: SmallFilmCardProps): JSX.Element => (
-  <article className="small-film-card catalog__films-card">
-    <div className="small-film-card__image">
-      <img src={film.preview} alt="" width="280" height="175" />
-    </div>
-    <h3 className="small-film-card__title">
-      <a className="small-film-card__link" href="film-page.html">
-        {film.name}
-      </a>
-    </h3>
-  </article>
-);
+  film,
+  changeSelectedFilm
+}: SmallFilmCardProps): JSX.Element => {
+
+  const handleMouseEnter = (event: MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    changeSelectedFilm(film.id);
+  };
+
+  const handleMouseLeave = (event: MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    changeSelectedFilm(null);
+  };
+
+  return (
+    <article
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className="small-film-card catalog__films-card"
+    >
+      <div className="small-film-card__image">
+        <img src={film.preview} alt="" width="280" height="175" />
+      </div>
+      <h3 className="small-film-card__title">
+        <a className="small-film-card__link" href="film-page.html">
+          {film.name}
+        </a>
+      </h3>
+    </article>
+  );
+};
 
 export default FilmCard;
