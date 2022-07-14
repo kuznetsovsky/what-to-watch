d41621d827b0d3ff0e109c19ca97dbc9df4dbf11
Mit'ka Kuznetsov <www.kuznetsovsky@gmail.com>
2022-07-14 22:23:38 +0300

    Выводит данные для страницы избранных фильмов из моков

--- project/src/pages/my-list-page/my-list-page.tsx
+++ project/src/pages/my-list-page/my-list-page.tsx
@@ -1,7 +1,19 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
+// Others
+import FilmsList from '../../components/films-list/films-list';
+import { favorites } from '../../mocks/favorites';
+import { FilmsType } from '../../types/film-types';
+
+// Props
+type MyListPageProps = {
+  favoritesFilms: FilmsType;
+};
+
 // Component
-const MyListPage = () => (
+const MyListPage = ({
+  favoritesFilms
+}: MyListPageProps): JSX.Element => (
   <div className="user-page">
     <header className="page-header user-page__head">
       <div className="logo">
@@ -28,91 +40,7 @@ const MyListPage = () => (
 
     <section className="catalog">
       <h2 className="catalog__title visually-hidden">Catalog</h2>
-
-      <div className="catalog__films-list">
-        <article className="small-film-card catalog__films-card">
-          <div className="small-film-card__image">
-            <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175" />
-          </div>
-          <h3 className="small-film-card__title">
-            <a className="small-film-card__link" href="film-page.html">Fantastic Beasts: The Crimes of Grindelwald</a>
-          </h3>
-        </article>
-
-        <article className="small-film-card catalog__films-card">
-          <div className="small-film-card__image">
-            <img src="img/bohemian-rhapsody.jpg" alt="Bohemian Rhapsody" width="280" height="175" />
-          </div>
-          <h3 className="small-film-card__title">
-            <a className="small-film-card__link" href="film-page.html">Bohemian Rhapsody</a>
-          </h3>
-        </article>
-
-        <article className="small-film-card catalog__films-card">
-          <div className="small-film-card__image">
-            <img src="img/macbeth.jpg" alt="Macbeth" width="280" height="175" />
-          </div>
-          <h3 className="small-film-card__title">
-            <a className="small-film-card__link" href="film-page.html">Macbeth</a>
-          </h3>
-        </article>
-
-        <article className="small-film-card catalog__films-card">
-          <div className="small-film-card__image">
-            <img src="img/aviator.jpg" alt="Aviator" width="280" height="175" />
-          </div>
-          <h3 className="small-film-card__title">
-            <a className="small-film-card__link" href="film-page.html">Aviator</a>
-          </h3>
-        </article>
-
-
-        <article className="small-film-card catalog__films-card">
-          <div className="small-film-card__image">
-            <img src="img/we-need-to-talk-about-kevin.jpg" alt="We need to talk about Kevin" width="280" height="175" />
-          </div>
-          <h3 className="small-film-card__title">
-            <a className="small-film-card__link" href="film-page.html">We need to talk about Kevin</a>
-          </h3>
-        </article>
-
-        <article className="small-film-card catalog__films-card">
-          <div className="small-film-card__image">
-            <img src="img/what-we-do-in-the-shadows.jpg" alt="What We Do in the Shadows" width="280" height="175" />
-          </div>
-          <h3 className="small-film-card__title">
-            <a className="small-film-card__link" href="film-page.html">What We Do in the Shadows</a>
-          </h3>
-        </article>
-
-        <article className="small-film-card catalog__films-card">
-          <div className="small-film-card__image">
-            <img src="img/revenant.jpg" alt="Revenant" width="280" height="175" />
-          </div>
-          <h3 className="small-film-card__title">
-            <a className="small-film-card__link" href="film-page.html">Revenant</a>
-          </h3>
-        </article>
-
-        <article className="small-film-card catalog__films-card">
-          <div className="small-film-card__image">
-            <img src="img/johnny-english.jpg" alt="Johnny English" width="280" height="175" />
-          </div>
-          <h3 className="small-film-card__title">
-            <a className="small-film-card__link" href="film-page.html">Johnny English</a>
-          </h3>
-        </article>
-
-
-        <article className="small-film-card catalog__films-card">
-          <div className="small-film-card__image">
-            <img src="img/shutter-island.jpg" alt="Shutter Island" width="280" height="175" />
-          </div>
-          <h3 className="small-film-card__title">
-            <a className="small-film-card__link" href="film-page.html">Shutter Island</a>
-          </h3>
-        </article>
-      </div>
+      <FilmsList films={favoritesFilms} />
     </section>
 
     <footer className="page-footer">
@@ -131,4 +59,10 @@ const MyListPage = () => (
   </div>
 );
 
+// Default Props
+MyListPage.defaultProps = {
+  favoritesFilms: favorites,
+};
+
+// Export
 export default MyListPage;
