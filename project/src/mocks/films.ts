f42d85a8332141fc080e665ec41fdfc1b1800d34
Mit'ka Kuznetsov <www.kuznetsovsky@gmail.com>
2022-07-14 21:53:36 +0300

    Выносит моки с фильмами в отдельную директорию

---
+++ project/src/mocks/films.ts
@@ -0,0 +1,102 @@
+export const films = [
+  {
+    id: 'ID-1',
+    name: 'Fantastic Beasts: The Crimes of Grindelwald',
+    preview: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
+  },
+  {
+    id: 'ID-2',
+    name: 'Bohemian Rhapsody',
+    preview: 'img/bohemian-rhapsody.jpg',
+  },
+  {
+    id: 'ID-3',
+    name: 'Macbeth',
+    preview: 'img/macbeth.jpg',
+  },
+  {
+    id: 'ID-4',
+    name: 'Aviator',
+    preview: 'img/aviator.jpg',
+  },
+  {
+    id: 'ID-5',
+    name: 'We need to talk about Kevin',
+    preview: 'img/we-need-to-talk-about-kevin.jpg',
+  },
+  {
+    id: 'ID-6',
+    name: 'What We Do in the Shadows',
+    preview: 'img/what-we-do-in-the-shadows.jpg',
+  },
+  {
+    id: 'ID-7',
+    name: 'Revenant',
+    preview: 'img/revenant.jpg',
+  },
+  {
+    id: 'ID-8',
+    name: 'Johnny English',
+    preview: 'img/johnny-english.jpg',
+  },
+  {
+    id: 'ID-9',
+    name: 'Shutter Island',
+    preview: 'img/shutter-island.jpg',
+  },
+  {
+    id: 'ID-10',
+    name: 'Pulp Fiction',
+    preview: 'img/pulp-fiction.jpg',
+  },
+  {
+    id: 'ID-11',
+    name: 'No Country for Old Men',
+    preview: 'img/no-country-for-old-men.jpg',
+  },
+  {
+    id: 'ID-12',
+    name: 'Snatch',
+    preview: 'img/snatch.jpg',
+  },
+  {
+    id: 'ID-13',
+    name: 'Moonrise Kingdom',
+    preview: 'img/moonrise-kingdom.jpg',
+  },
+  {
+    id: 'ID-14',
+    name: 'Seven Years in Tibet',
+    preview: 'img/seven-years-in-tibet.jpg',
+  },
+  {
+    id: 'ID-15',
+    name: 'Midnight Special',
+    preview: 'img/midnight-special.jpg',
+  },
+  {
+    id: 'ID-16',
+    name: 'War of the Worlds',
+    preview: 'img/war-of-the-worlds.jpg',
+  },
+  {
+    id: 'ID-17',
+    name: 'Dardjeeling Limited',
+    preview: 'img/dardjeeling-limited.jpg',
+  },
+  {
+    id: 'ID-18',
+    name: 'Orlando',
+    preview: 'img/orlando.jpg',
+  },
+  {
+    id: 'ID-19',
+    name: 'Mindhunter',
+    preview: 'img/mindhunter.jpg',
+  },
+  {
+    id: 'ID-20',
+    name: 'Midnight Special',
+    preview: 'img/midnight-special.jpg',
+  },
+];
