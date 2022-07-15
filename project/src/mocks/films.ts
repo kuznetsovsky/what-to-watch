6021a1bfeda74f33afa8bc90417de0288c1c4536
Mit'ka Kuznetsov <www.kuznetsovsky@gmail.com>
2022-07-15 20:26:17 +0300

    Добавляет ссылки к видео для моков с фильмами

--- project/src/mocks/films.ts
+++ project/src/mocks/films.ts
@@ -1,104 +1,126 @@
 import { FilmsType } from '../types/film-types';
 
+const VIDEO_SRC = 'https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4';
+
 export const films: FilmsType = [
   {
     id: 'ID-1',
     name: 'Fantastic Beasts: The Crimes of Grindelwald',
     preview: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
+    videoSrc: VIDEO_SRC,
   },
   {
     id: 'ID-2',
     name: 'Bohemian Rhapsody',
     preview: 'img/bohemian-rhapsody.jpg',
+    videoSrc: VIDEO_SRC,
   },
   {
     id: 'ID-3',
     name: 'Macbeth',
     preview: 'img/macbeth.jpg',
+    videoSrc: VIDEO_SRC,
   },
   {
     id: 'ID-4',
     name: 'Aviator',
     preview: 'img/aviator.jpg',
+    videoSrc: VIDEO_SRC,
   },
   {
     id: 'ID-5',
     name: 'We need to talk about Kevin',
     preview: 'img/we-need-to-talk-about-kevin.jpg',
+    videoSrc: VIDEO_SRC,
   },
   {
     id: 'ID-6',
     name: 'What We Do in the Shadows',
     preview: 'img/what-we-do-in-the-shadows.jpg',
+    videoSrc: VIDEO_SRC,
   },
   {
     id: 'ID-7',
     name: 'Revenant',
     preview: 'img/revenant.jpg',
+    videoSrc: VIDEO_SRC,
   },
   {
     id: 'ID-8',
     name: 'Johnny English',
     preview: 'img/johnny-english.jpg',
+    videoSrc: VIDEO_SRC,
   },
   {
     id: 'ID-9',
     name: 'Shutter Island',
     preview: 'img/shutter-island.jpg',
+    videoSrc: VIDEO_SRC,
   },
   {
     id: 'ID-10',
     name: 'Pulp Fiction',
     preview: 'img/pulp-fiction.jpg',
+    videoSrc: VIDEO_SRC,
   },
   {
     id: 'ID-11',
     name: 'No Country for Old Men',
     preview: 'img/no-country-for-old-men.jpg',
+    videoSrc: VIDEO_SRC,
   },
   {
     id: 'ID-12',
     name: 'Snatch',
     preview: 'img/snatch.jpg',
+    videoSrc: VIDEO_SRC,
   },
   {
     id: 'ID-13',
     name: 'Moonrise Kingdom',
     preview: 'img/moonrise-kingdom.jpg',
+    videoSrc: VIDEO_SRC,
   },
   {
     id: 'ID-14',
     name: 'Seven Years in Tibet',
     preview: 'img/seven-years-in-tibet.jpg',
+    videoSrc: VIDEO_SRC,
   },
   {
     id: 'ID-15',
     name: 'Midnight Special',
     preview: 'img/midnight-special.jpg',
+    videoSrc: VIDEO_SRC,
   },
   {
     id: 'ID-16',
     name: 'War of the Worlds',
     preview: 'img/war-of-the-worlds.jpg',
+    videoSrc: VIDEO_SRC,
   },
   {
     id: 'ID-17',
     name: 'Dardjeeling Limited',
     preview: 'img/dardjeeling-limited.jpg',
+    videoSrc: VIDEO_SRC,
   },
   {
     id: 'ID-18',
     name: 'Orlando',
     preview: 'img/orlando.jpg',
+    videoSrc: VIDEO_SRC,
   },
   {
     id: 'ID-19',
     name: 'Mindhunter',
     preview: 'img/mindhunter.jpg',
+    videoSrc: VIDEO_SRC,
   },
   {
     id: 'ID-20',
     name: 'Midnight Special',
     preview: 'img/midnight-special.jpg',
+    videoSrc: VIDEO_SRC,
   },
 ];
